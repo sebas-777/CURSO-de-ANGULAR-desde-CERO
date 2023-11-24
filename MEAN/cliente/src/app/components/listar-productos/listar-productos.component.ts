d106b214380b6a42a5a76df964636e27989ec51e
Sebastian Castro <scastro@qvision.com.co>
2023-11-24 15:06:31 -0500

    creacion de estructura

---
+++ MEAN/cliente/src/app/components/listar-productos/listar-productos.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-listar-productos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './listar-productos.component.html',
+  styleUrl: './listar-productos.component.css'
+})
+export class ListarProductosComponent {
+
+}
